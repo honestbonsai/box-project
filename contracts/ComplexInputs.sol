456c472a645c968e5d760195154e8f45c9200cd6
Erin Shaben <eshaben@icloud.com>
2020-09-24 19:46:24 -0600

    add a new contract with long fn inputs

---
+++ contracts/ComplexInputs.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.7.0;
+
+contract SimpleStorage {
+    event StorageSet(string _message);
+
+    uint256 public storedData;
+    uint256 public storeduint1 = 15;
+
+    bytes16 public string1 = "test1";
+    bytes32 public string2 = "test1236";
+    string public string3 = "lets string something";
+
+    function set(uint256 x) public {
+        storedData = x;
+
+        emit StorageSet("Data stored successfully!");
+    }
+
+    function setAllData(
+        uint256 x,
+        uint256 storeduint1letsmakethisareallylongnameyay,
+        bytes16 string1yayanotherlongname,
+        bytes32 string2andanotherone,
+        string string3andanother
+    ) public {
+        storedData = x;
+        storeduint1 = storeduint1letsmakethisareallylongnameyay;
+        string1 = string1yayanotherlongname;
+        string2 = string2andanotherone;
+        string3 = string3andanother;
+    }
+}
