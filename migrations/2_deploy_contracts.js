1fb6fea0e657974546dd06c75e7f353d350fe7a6
Erin Shaben <eshaben@icloud.com>
2020-10-12 17:02:10 -0600

    add new contracts to migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,10 +2,16 @@ const SimpleStorage = artifacts.require("SimpleStorage");
 const TutorialToken = artifacts.require("TutorialToken");
 const ComplexStorage = artifacts.require("ComplexStorage");
 const ComplexInputs = artifacts.require("ComplexInputs");
+const SimpleInputs = artifacts.require("SimpleInputs");
+const ArrayContract = artifacts.require("ArrayContract");
+const Crowdfunding = artifacts.require("Crowdfunding");
 
 module.exports = function (deployer) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(TutorialToken);
   deployer.deploy(ComplexStorage);
   deployer.deploy(ComplexInputs);
+  deployer.deploy(SimpleInputs);
+  deployer.deploy(ArrayContract);
+  deployer.deploy(Crowdfunding);
 };
