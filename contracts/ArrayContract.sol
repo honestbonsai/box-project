ce2bc6a4b47f6fb7df1280b4e836eb14e01cebfe
Erin Shaben <eshaben@icloud.com>
2020-10-27 23:38:28 -0600

    trigger a build

--- contracts/ArrayContract.sol
+++ contracts/ArrayContract.sol
@@ -49,7 +49,6 @@ contract ArrayContract {
     }
 
     function setFlagPairFn(function() external setNewFlagPair) public {
-        // access to a non-existing index will throw an exception
         setNewFlagPair();
     }
 
