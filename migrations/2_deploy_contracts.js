1971b70a6dbaacce08ae6305e7f76d891f0cc0eb
Erin Shaben <eshaben@icloud.com>
2020-11-24 23:01:48 -0700

    update migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,17 +1,19 @@
 const SimpleStorage = artifacts.require("SimpleStorage");
+const UnnamedInputs = artifacts.require("UnnamedInputs");
+const ArrayContract = artifacts.require("ArrayContract");
 // const TutorialToken = artifacts.require("TutorialToken");
-const ComplexStorage = artifacts.require("ComplexStorage");
+// const ComplexStorage = artifacts.require("ComplexStorage");
 // const ComplexInputs = artifacts.require("ComplexInputs");
 // const SimpleInputs = artifacts.require("SimpleInputs");
-// const ArrayContract = artifacts.require("ArrayContract");
 // const CrowdFunding = artifacts.require("CrowdFunding");
 
 module.exports = function (deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(UnnamedInputs);
+  deployer.deploy(ArrayContract);
   // deployer.deploy(TutorialToken);
-  deployer.deploy(ComplexStorage);
+  // deployer.deploy(ComplexStorage);
   // deployer.deploy(ComplexInputs);
   // deployer.deploy(SimpleInputs);
-  // deployer.deploy(ArrayContract);
   // deployer.deploy(CrowdFunding);
 };
