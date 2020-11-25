3e22ed22c00c251e5a4ac7e61da81334c280cf85
Erin Shaben <eshaben@icloud.com>
2020-11-24 23:00:33 -0700

    add unnamed test contract

---
+++ contracts/UnnamedInputs.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.7.0;
+pragma experimental ABIEncoderV2;
+
+contract ComplexInputs {
+    event StorageSet(string _message);
+
+    function unnamedParameter(uint256) public {
+        emit StorageSet("Data stored successfully!");
+    }
+
+    function unnamedArrayParameter(uint256[1] memory) public {
+        emit StorageSet("Data stored successfully!");
+    }
+
+    function multipleUnnamedParameters(uint256[1] memory, uint256) public {
+        emit StorageSet("Data stored successfully!");
+    }
+
+    function mixedNamedAndUnnamedParameters(uint256[1] memory, uint256 x)
+        public
+    {
+        emit StorageSet("Data stored successfully!");
+    }
+
+    function mixedNamedAndUnnamedParameters2(uint256[1] memory x, uint256)
+        public
+    {
+        emit StorageSet("Data stored successfully!");
+    }
+}
