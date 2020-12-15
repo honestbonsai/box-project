57ac42cd9ce5e6137565b66700d1fefee7df1e11
Erin Shaben <eshaben@icloud.com>
2020-12-14 22:35:48 -0700

    add ComplexInputs contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,19 +1,19 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
-const UnnamedInputs = artifacts.require("UnnamedInputs");
-const ArrayContract = artifacts.require("ArrayContract");
+const SimpleStorage = artifacts.require('SimpleStorage');
+const UnnamedInputs = artifacts.require('UnnamedInputs');
+const ArrayContract = artifacts.require('ArrayContract');
 // const TutorialToken = artifacts.require("TutorialToken");
 // const ComplexStorage = artifacts.require("ComplexStorage");
-// const ComplexInputs = artifacts.require("ComplexInputs");
+const ComplexInputs = artifacts.require('ComplexInputs');
 // const SimpleInputs = artifacts.require("SimpleInputs");
 // const CrowdFunding = artifacts.require("CrowdFunding");
 
 module.exports = function (deployer) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(UnnamedInputs);
-  deployer.deploy(ArrayContract);
-  // deployer.deploy(TutorialToken);
-  // deployer.deploy(ComplexStorage);
-  // deployer.deploy(ComplexInputs);
-  // deployer.deploy(SimpleInputs);
-  // deployer.deploy(CrowdFunding);
+    deployer.deploy(SimpleStorage);
+    deployer.deploy(UnnamedInputs);
+    deployer.deploy(ArrayContract);
+    // deployer.deploy(TutorialToken);
+    // deployer.deploy(ComplexStorage);
+    deployer.deploy(ComplexInputs);
+    // deployer.deploy(SimpleInputs);
+    // deployer.deploy(CrowdFunding);
 };
